2c86db4d3c448aaa32ad505990689cd4047154c6
MdAlSiam <md.al.siam.008@gmail.com>
2021-07-24 09:49:31 +0600

    Added: Queue Using Stack

--- CodeNotes.cpp
+++ CodeNotes.cpp
@@ -1177,6 +1177,55 @@ void merge_sort(ll* ara, ll lo, ll hi) {
     }
 }
 
+// Queue Using Stack
+
+class MyQueue {
+    // Queue Using Stack
+public:
+    /** Initialize your data structure here. */
+    stack<int> queueStack;
+    stack<int> temp;
+
+    MyQueue() {
+        while (!queueStack.empty()) queueStack.pop();
+        while (!temp.empty()) temp.pop();
+    }
+    
+    /** Push element x to the back of queue. */
+    void push(int x) {
+        while (!temp.empty()) temp.pop();
+        while (!queueStack.empty()) {
+            int element = queueStack.top();
+            temp.push(element);
+            queueStack.pop();
+        }
+        queueStack.push(x);
+        while (!temp.empty()) {
+            int element = temp.top();
+            queueStack.push(element);
+            temp.pop();
+        }
+    }
+    
+    /** Removes the element from in front of queue and returns that element. */
+    int pop() {
+        int element = queueStack.top();
+        queueStack.pop();
+        return element;
+    }
+    
+    /** Get the front element. */
+    int peek() {
+        int element = queueStack.top();
+        return element;
+    }
+    
+    /** Returns whether the queue is empty. */
+    bool empty() {
+        return queueStack.empty();
+    }
+};
+
 void solve() {
     printf("This is a test case.\n");
 } 
@@ -1186,5 +1235,3 @@ int32_t main() {
     scl(test);
     while (test--) solve();
 }
-
-// This is a change 
