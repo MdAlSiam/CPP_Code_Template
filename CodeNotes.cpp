be0a6f28ce3e9a22ae28b940cec21e0bed9a82aa
MdAlSiam <md.al.siam.008@gmail.com>
2021-07-26 20:25:23 +0600

    Added: Building a Balanced BST from a Sorted Array

--- CodeNotes.cpp
+++ CodeNotes.cpp
@@ -1226,6 +1226,38 @@ public:
     }
 };
 
+// Build a Balanced Binary Tree From a Sorted Array
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+private:
+    TreeNode* go(vector<int>& nums, int start, int stop) {
+        if (start > stop) {
+            return nullptr;
+        }
+
+        int mid = start + (stop - start) / 2;
+
+        TreeNode* node = new TreeNode(nums[mid]);
+        node->left = go(nums, start, mid-1);
+        node->right = go(nums, mid+1, stop);
+
+        return node;
+    }
+public:
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        return go(nums, 0, nums.size()-1);
+    }
+};
+
 void solve() {
     printf("This is a test case.\n");
 } 
