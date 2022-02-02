6f38f23d4bbc426f22d196d332ae5f2c11e7b33c
Md. Al Siam <highambitioussiam@gmail.com>
2022-02-03 01:25:38 +0600

    [038] Sliding Window Technique

--- 00.CodeNotes.cpp
+++ 00.CodeNotes.cpp
@@ -1590,6 +1590,33 @@ public:
     }
 };
 
+// [038] Sliding Window Technique
+// LC 438: https://leetcode.com/problems/find-all-anagrams-in-a-string/
+
+vector<int> findAnagrams(string s, string p) {
+    vector<int> freq_p(26, 0);
+    vector<int> window(26, 0);
+    vector<int> ans;
+    ans.clear();
+
+    if (s.length() < p.length()) return ans;
+    
+    for (int i = 0; i < p.length(); i++) {
+        freq_p[p[i]-'a']++;
+        window[s[i]-'a']++;
+    }
+    if (freq_p == window) ans.push_back(0);
+    
+    for(int i = p.length(); i < s.length(); i++){
+        window[s[i-p.length()]-'a']--;
+        window[s[i]-'a']++;
+        
+        if (freq_p == window) ans.push_back(i-p.length()+1);
+    }
+
+    return ans;
+}
+
 void solve() {
     printf("This is a test case.\n");
 } 
