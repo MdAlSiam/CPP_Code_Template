58e444b4add429ca74e4a902631a4ecce0685d6b
Md. Al Siam <highambitioussiam@gmail.com>
2022-04-01 02:36:32 +0600

    [041] Binary Search on Answer Range

--- 00.CodeNotes.cpp
+++ 00.CodeNotes.cpp
@@ -1671,6 +1671,61 @@ public:
     }
 };
 
+// [041] Binary Search on Answer Range
+// LeetCode 410. Split Array Largest Sum
+
+class Solution {
+    int isPossible(vector<int>& nums, int mid) {
+        // Kounts how many subarrays are possible in nums with sum less than or equal mid
+
+        int kount = 0;
+        int tempsum = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (tempsum+nums[i] <= mid) {
+                tempsum += nums[i];
+            }
+            else {
+                kount++;
+                tempsum = nums[i];
+            }
+        }
+        kount++; // The last subsequnece
+
+        return kount;
+    }
+public:
+    int splitArray(vector<int>& nums, int m) {
+
+        int lo = 0;
+        int hi = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            lo = max(lo, nums[i]);
+            hi += nums[i];
+        }
+
+        int mid = 0;
+        int ans = 0;
+
+        while (lo <= hi) {
+            mid = (lo+hi)/2;
+
+            int kountOfSegment = isPossible(nums, mid);
+
+            if (kountOfSegment <= m) {
+                ans = mid;
+                hi = mid-1;
+            }
+            else {
+                lo = mid+1;
+            }
+        }
+
+        return ans;
+    }
+};
+
 void solve() {
     printf("This is a test case.\n");
 } 
