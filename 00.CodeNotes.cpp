25811b20433daf313b40577669b7c6930178c9cd
Md. Al Siam <highambitioussiam@gmail.com>
2022-08-09 11:26:37 +0600

    [Updated] LIS Two Approaches - LeetCode 400

--- 00.CodeNotes.cpp
+++ 00.CodeNotes.cpp
@@ -414,6 +414,50 @@ int at_main_LIS() {
     }
 }
 
+class Solution {
+public:
+    // LIS Two Approaches - LeetCode 400
+    int lengthOfLIS_DP(vector<int>& nums) {
+        // TC: O(n^2)
+        int dp[nums.size()];
+
+        for (int i = 0; i < nums.size(); i++) {
+            dp[i] = 1;
+        }
+
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = i+1; j < nums.size(); j++) {
+                if (nums[j] > nums[i]) {
+                    dp[j] = max(dp[j], dp[i]+1);
+                }
+            }
+        }
+
+        int ans = 1;
+        for (int i = 0; i < nums.size(); i++) {
+            ans = max(ans, dp[i]);
+        }
+
+        return ans;
+    }
+
+    int lengthOfLIS_DP(vector<int>& nums) {
+        // TC: O(n*log n)
+        vector<int> tail;
+
+        for (int num : nums) {
+            if (tail.empty() || num > tail.back()) {
+                tail.push_back(num);
+            }
+            else {
+                tail[lower_bound(tail.begin(), tail.end(), num) - tail.begin()] = num;
+            }
+        }
+
+        return tail.size();
+    }
+};
+
 // [014] Longest Common Subsequence
 
 /*
